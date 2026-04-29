a0e5bb2ac5bfce6c1ae918860f5e4062aa77023d
Ruan <lucasruanpracf@hotmail.com>
2026-04-29 13:49:03 -0300

    User insert, User update, User updateData, User delete

--- src/main/java/com/ruan/booktracker/book_tracker_api/services/UserService.java
+++ src/main/java/com/ruan/booktracker/book_tracker_api/services/UserService.java
@@ -23,4 +23,29 @@ public class UserService {
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
+
+    public User insert(User obj) {
+        return repository.save(obj);
+    }
+
+    public User update(Long id, User obj) {
+        User entity = repository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(id));
+
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setEmail(obj.getEmail());
+        entity.setPassword(obj.getPassword());
+    }
+
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException(id);
+        }
+        repository.deleteById(id);
+    }
 }
