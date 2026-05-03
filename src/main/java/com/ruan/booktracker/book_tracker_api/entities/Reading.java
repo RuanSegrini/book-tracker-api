d3ae596cfe987773be69757e8adc042848ac571a
Ruan <lucasruanpracf@hotmail.com>
2026-05-03 12:14:53 -0300

    feat: create Reading entity with relationships

---
+++ src/main/java/com/ruan/booktracker/book_tracker_api/entities/Reading.java
@@ -0,0 +1,125 @@
+package com.ruan.booktracker.book_tracker_api.entities;
+
+import com.ruan.booktracker.book_tracker_api.entities.enums.ReadingStatus;
+import jakarta.persistence.*;
+
+import jakarta.validation.constraints.Min;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_reading")
+public class Reading implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    @Min(value = 1)
+    private Integer currentPage;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReadingStatus status;
+
+    @Column(nullable = false)
+    private LocalDateTime startedAt;
+
+    private LocalDateTime finishedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @PrePersist
+    public void prePersist() {
+        startedAt = LocalDateTime.now();
+    }
+
+
+    public Reading() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(Integer currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public ReadingStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ReadingStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getStartedAt() {
+        return startedAt;
+    }
+
+    public void setStartedAt(LocalDateTime startedAt) {
+        this.startedAt = startedAt;
+    }
+
+    public LocalDateTime getFinishedAt() {
+        return finishedAt;
+    }
+
+    public void setFinishedAt(LocalDateTime finishedAt) {
+        this.finishedAt = finishedAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public boolean isCompleted() {
+        return status == ReadingStatus.COMPLETED;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this == o) return true;
+        if (!(o instanceof Reading reading)) return false;
+        if(id == null || reading.id == null) return false;
+        return Objects.equals(id, reading.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
