512c6175691a5355a36722e43758f91920eeb896
Ruan <lucasruanpracf@hotmail.com>
2026-05-06 11:51:28 -0300

    feat: add Favorite entity with user-book relationship

---
+++ src/main/java/com/ruan/booktracker/book_tracker_api/entities/Favorite.java
@@ -0,0 +1,83 @@
+package com.ruan.booktracker.book_tracker_api.entities;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(
+        name = "tb_favorite",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"user_id", "book_id"})
+)
+public class Favorite implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id")
+    private Book book;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+
+    public Favorite() {}
+
+    public Favorite(Long id, User user, Book book) {
+        this.id = id;
+        this.user = user;
+        this.book = book;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Favorite that)) return false;
+        if (id == null || that.id == null) return false;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
