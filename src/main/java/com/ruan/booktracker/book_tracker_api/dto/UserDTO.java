02986d242687065ccd92d58d0cdc3fdc30595af1
Ruan <lucasruanpracf@hotmail.com>
2026-05-07 22:19:42 -0300

    UserDTO created

---
+++ src/main/java/com/ruan/booktracker/book_tracker_api/dto/UserDTO.java
@@ -0,0 +1,37 @@
+package com.ruan.booktracker.book_tracker_api.dto;
+
+import com.ruan.booktracker.book_tracker_api.entities.User;
+
+public class UserDTO {
+
+    private Long id;
+    private String name;
+    private String email;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public UserDTO(User entity) {
+        id = entity.getId();
+        name = entity.getName();
+        email = entity.getEmail();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
