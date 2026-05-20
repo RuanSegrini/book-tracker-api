5662d1499f117c56ddd7b71b7486de81728e0f2d
Ruan <lucasruanpracf@hotmail.com>
2026-05-19 22:14:14 -0300

    fix application and add docker

--- src/main/java/com/ruan/booktracker/book_tracker_api/repositories/ReadingRepository.java
+++ src/main/java/com/ruan/booktracker/book_tracker_api/repositories/ReadingRepository.java
@@ -3,5 +3,5 @@ package com.ruan.booktracker.book_tracker_api.repositories;
 import com.ruan.booktracker.book_tracker_api.entities.Reading;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReadingRepository extends JpaRepository<Reading, Long> {
+public interface  ReadingRepository extends JpaRepository<Reading, Long> {
 }
